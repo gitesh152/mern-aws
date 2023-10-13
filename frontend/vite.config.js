1c786e25ab002dc688b59474aab65e096e0a4294
gitesh152 <gitesh152@gmail.com>
2023-10-13 17:56:15 +0530

    mern-aws

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000"  // Replace with your backend API's URL
-      // "/api": "https://mern-aws.onrender.com"  // Replace with your backend API's URL
+      // "/api": "http://localhost:5000"  // Replace with your backend API's URL
+      "/api": "https://mern-aws.onrender.com"  // Replace with your backend API's URL
     }
   }
 })
