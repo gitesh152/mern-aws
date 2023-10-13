61a75cfaf0d19a6e5e318643e504af4541c16fee
gitesh152 <gitesh152@gmail.com>
2023-10-13 16:48:38 +0530

    mern-aws

--- frontend/src/FileDropzone.jsx
+++ frontend/src/FileDropzone.jsx
@@ -16,7 +16,7 @@ import axios from "axios";
 import UploadPreview from "./UploadPreview";
 
 // var API_URL = `http://localhost:5000/api/v1`;
-var API_URL = `http://ec2-3-110-148-151.ap-south-1.compute.amazonaws.com:5000/api/v1`;
+var API_URL = `https://mern-aws.onrender.com/api/v1`;
 
 const FileDropzone = () => {
     const [file, setFile] = useState(null);
